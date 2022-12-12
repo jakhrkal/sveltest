4122816e484a2c09441e18ac203d21f9f069b04a
Jakub Hrkal <jakhrkal@gmail.com>
2022-12-12 22:28:05 +0000

    OpenAPI TypeScript generator

---
+++ src/lib/generated/api/apis/index.ts
@@ -0,0 +1,4 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './GameControllerApi';
+export * from './UserControllerApi';
---
+++ src/lib/generated/api/models/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ShowGame';
+export * from './ShowUser';
+export * from './StoreGame';
+export * from './StoreUser';
